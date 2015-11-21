8992a9ee7b920ec005003c688125d41277037a08
Scott K Logan <logans@cottsay.net>
2015-11-21 17:55:31 -0600

    Break out 20/60 minute rates

--- summary.php
+++ summary.php
@@ -10,7 +10,9 @@ try
 
     $maxbreak = $hl->getMaximumBreak();
 
-    $tenminavg = $hl->countQsos(array(0 => 'LoggedAt > (UTC_TIMESTAMP() - INTERVAL 10 MINUTE) AND EventID=' . $hl->currEvent()));
+    $twentymincnt = $hl->countQsos(array(0 => 'LoggedAt > (UTC_TIMESTAMP() - INTERVAL 20 MINUTE) AND EventID=' . $hl->currEvent()));
+
+    $sixtymincnt = $hl->countQsos(array(0 => 'LoggedAt > (UTC_TIMESTAMP() - INTERVAL 60 MINUTE) AND EventID=' . $hl->currEvent()));
 }
 catch(HardloggerException $e)
 {
@@ -28,7 +30,8 @@ include("header.php");
 <tr><td>First QSO:</td><td><?php echo $stats['first'] ?></td></tr>
 <tr><td>Most Recent QSO:</td><td><?php echo $stats['last'] ?></td></tr>
 <tr><td>Longest Break:</td><td><?php echo $maxbreak == '' ? '' : $maxbreak . ' hrs'  ?></td></tr>
-<tr><td>10 Minute Average:</td><td><?php echo $tenminavg * 6 ?> QSOs/hr</td></tr>
+<tr><td>20 Minute Rate:</td><td><?php echo $twentymincnt * 3 ?> QSOs/hr</td></tr>
+<tr><td>60 Minute Rate:</td><td><?php echo $sixtymincnt ?> QSOs/hr</td></tr>
 <tr><td>Current Score:</td><td><?php echo ((2 * $stats['count']) *  $stats['sections']) ?></td></tr>
 
 </table>
